1efe04facd5edce1b0d3815e752e3d4a20d09fa4
Wojciech Trocki <wtrocki@redhat.com>
2018-12-12 17:33:56 +0000

    Fix: preloading for router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: 'update-item', loadChildren: './pages/update-item/update-item.module#UpdateItemPageModule' },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
