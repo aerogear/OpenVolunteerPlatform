1632748117d877a87f4bea1de14cdbe825eb860a
Wojciech Trocki <wtrocki@redhat.com>
2018-12-18 11:32:42 +0000

    Add eager package routing (for production only)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-// TODO replace load children with loading all modules instantly
+// Disable lazy loading
+import { HomePageModule } from './pages/home/home.module';
+
+export function loadHomePageModule() {
+  return HomePageModule;
+}
+
+import { NewItemPageModule } from './pages/new-item/new-item.module';
+
+export function loadNewItemPageModule() {
+  return NewItemPageModule;
+}
+
+import { UpdateItemPageModule } from './pages/update-item/update-item.module';
+
+export function loadUpdateItemPageModule() {
+  return UpdateItemPageModule;
+}
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'new-item', loadChildren: './pages/new-item/new-item.module#NewItemPageModule' },
-  { path: 'update-item', loadChildren: './pages/update-item/update-item.module#UpdateItemPageModule' },
+  { path: 'home', loadChildren: loadHomePageModule },
+  { path: 'new-item', loadChildren: loadNewItemPageModule },
+  { path: 'update-item', loadChildren: loadUpdateItemPageModule },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  imports: [
+    HomePageModule,
+    NewItemPageModule,
+    UpdateItemPageModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
