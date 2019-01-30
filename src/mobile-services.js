40e78ba4d6a924785056933ae0344502150db2fd
Wojciech Trocki <wtrocki@redhat.com>
2019-01-30 10:25:45 +0000

    fix: Invalid config

--- src/mobile-services.js
+++ src/mobile-services.js
@@ -3,20 +3,7 @@ let config = {
   "namespace": "voyager",
   "clientId": "voyager-ionic-example",
   "services": [
-    {
-      "id": "417d12f1-1586-11e9-afb5-02cf927517aa",
-      "name": "keycloak",
-      "type": "keycloak",
-      "url": "https://keycloak-09271b-collab-project.comm2.skunkhenry.com/auth",
-      "config": {
-        "auth-server-url": "https://keycloak-09271b-collab-project.comm2.skunkhenry.com/auth",
-        "confidential-port": 0,
-        "public-client": true,
-        "realm": "voyager-testing",
-        "resource": "voyager-mobile-client",
-        "ssl-required": "external"
-      }
-    }
+    
   ]
 };
 
