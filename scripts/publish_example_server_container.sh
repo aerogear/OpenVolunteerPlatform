8f4eb8eaecc4f00157f48158466d84aa48205814
Vojtěch Sázel <wojta@users.noreply.github.com>
2019-02-05 11:52:17 +0100

    feat: Pushing example server docker container to Dockerhub  (#44)
    * feat: pushing container for server example
    * Addressing PR comments

---
+++ scripts/publish_example_server_container.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+[ -z "$CI" ] && echo "This script is meant to run only from CircleCI." && exit 1;
+[ -z "$DOCKERHUB_USERNAME" ] && echo "Undefined DOCKERHUB_USERNAME, skipping publish" && exit 1;
+
+docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+
+#use TAG env. variable to create the container with the given tag
+TAG="${TAG:-latest}"
+
+NAMESPACE="aerogear"
+CONTAINER="$NAMESPACE/voyager-server-example-task:$TAG"
+CONTAINER_LATEST="$NAMESPACE/voyager-server-example-task:latest"
+
+echo "Building docker container $CONTAINER"
+docker build -f Dockerfile -t "$CONTAINER" . && docker push "$CONTAINER" && \
+docker tag "$CONTAINER" "$CONTAINER_LATEST" && docker push "$CONTAINER_LATEST"
