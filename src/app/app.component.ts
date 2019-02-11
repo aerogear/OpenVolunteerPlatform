12cb95b8105b6bf0d2224a1ed0bc3a590195a3ac
Wojciech Trocki <wtrocki@redhat.com>
2019-02-11 17:55:50 +0000

    WIP: app.component integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,53 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  private user: String;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public auth: AuthService
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
+    this.initAuth();
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
+
+  private initAuth() {
+    return this.auth.init().then(() => {
+      return this.auth.authService.extract().loadUserProfile().success((profile) => {
+        this.user = profile.username;
+      });
+    }).catch((error) => {
+      if (error) {
+        // tslint:disable-next-line:no-console
+        console.info('Error when initializing auth', error);
+      }
+    });
+  }
+
+  login() {
+    if (this.auth.authService) {
+      if (this.auth.authService.isAuthenticated()) {
+        this.auth.authService.logout();
+      } else {
+        this.auth.authService.login();
+      }
+    }
+  }
+
 }
