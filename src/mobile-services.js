62f2cd6b54489ae51525e669392261cde804727c
Wojtek Trocki <wtrocki@redhat.com>
2019-02-22 11:04:26 +0000

    fix: remove invalid config

--- src/mobile-services.js
+++ src/mobile-services.js
@@ -2,20 +2,7 @@ let config = {
   "version": 1,
   "namespace": "voyager",
   "clientId": "voyager-ionic-example",
-  "services": [{
-    "id": "3a3e7a81-262a-11e9-bd5f-0af08791569c",
-    "name": "keycloak-5e98cf",
-    "type": "keycloak",
-    "url": "https://keycloak-09271b-collab-project.comm2.skunkhenry.com/auth",
-    "config": {
-      "realm": "voyager-testing",
-      "auth-server-url": "https://keycloak-09271b-collab-project.comm2.skunkhenry.com/auth",
-      "ssl-required": "none",
-      "resource": "voyager-mobile-client",
-      "public-client": true,
-      "confidential-port": 0
-    }
-  }]
+  "services": []
 };
 
 module.exports = config;
