6c424d56fcf2c1a4dc6aaa492cd7c6ef99fee188
piccolazucca <milena.zuccarelli@gmail.com>
2019-03-26 09:52:03 +0000

    minor clean up

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -10,29 +10,18 @@ import { Events } from "@ionic/angular";
 })
 export class SettingsPage implements OnInit {
   pushEnabled: any;
-  offlineToggleEnabled: any;
 
   constructor(private storage: Storage, public events: Events) {}
 
   async ngOnInit() {
     await this.storage.ready();
     const pushEnabledInStorage = await this.storage.get("pushEnabled");
-    const offlineToggleEnabledInStorage = await this.storage.get(
-      "offlineToggleEnabled"
-    );
 
     // Enable Push if there is no configuration in storage from user yet
     if (pushEnabledInStorage === null || pushEnabledInStorage) {
       this.pushEnabled = true;
     } else {
       this.pushEnabled = false;
-    }
-
-    // Enable Offline Toggle if there is no configuration in storage from user yet
-    if (offlineToggleEnabledInStorage === null || offlineToggleEnabledInStorage) {
-      this.offlineToggleEnabled = true;
-    } else {
-      this.offlineToggleEnabled = false;
     }
   }
 
@@ -41,14 +30,6 @@ export class SettingsPage implements OnInit {
 
     if (this.pushEnabled !== pushEnabledInStorage) {
       this.updateValue("pushEnabled", this.pushEnabled);
-    }
-  }
-
-  async toggleOfflineEnabled() {
-    const offlineToggleEnabledInStorage = await this.storage.get("offlineToggleEnabled");
-
-    if (this.offlineToggleEnabled !== offlineToggleEnabledInStorage) {
-      this.updateValue("offlineToggleEnabled", this.offlineToggleEnabled);
     }
   }
 
