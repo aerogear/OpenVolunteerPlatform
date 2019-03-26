83fbbb9736d45b610b9c7f0101e97b29034db358
piccolazucca <milena.zuccarelli@gmail.com>
2019-03-26 10:01:40 +0000

    reverted page formatting

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from "@angular/core";
-import { Storage } from "@ionic/storage";
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
-import { Events } from "@ionic/angular";
+import { Events } from '@ionic/angular';
 
 @Component({
-  selector: "app-settings",
-  templateUrl: "./settings.page.html",
-  styleUrls: ["./settings.page.scss"]
+  selector: 'app-settings',
+  templateUrl: './settings.page.html',
+  styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
-  pushEnabled: any;
+    pushEnabled: any;
 
-  constructor(private storage: Storage, public events: Events) {}
+  constructor(private storage: Storage,
+              public events: Events) {
+
+  }
 
   async ngOnInit() {
     await this.storage.ready();
-    const pushEnabledInStorage = await this.storage.get("pushEnabled");
+    const pushEnabledInStorage = await this.storage.get('pushEnabled');
 
     // Enable Push if there is no configuration in storage from user yet
     if (pushEnabledInStorage === null || pushEnabledInStorage) {
@@ -26,15 +29,15 @@ export class SettingsPage implements OnInit {
   }
 
   async togglePushEnabled() {
-    const pushEnabledInStorage = await this.storage.get("pushEnabled");
+    const pushEnabledInStorage = await this.storage.get('pushEnabled');
 
     if (this.pushEnabled !== pushEnabledInStorage) {
-      this.updateValue("pushEnabled", this.pushEnabled);
+      this.updateValue('pushEnabled', this.pushEnabled);
     }
   }
 
   private async updateValue(key: string, value: boolean) {
     await this.storage.set(key, value);
-    this.events.publish("settings:changed", key, value);
+    this.events.publish('settings:changed', key, value);
   }
 }
