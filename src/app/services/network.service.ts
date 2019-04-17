a5e955ad3f7433e05e62621db60af843f7f4c80c
Wojciech Trocki <wtrocki@redhat.com>
2019-04-17 13:59:38 +0200

    fix: add note

--- src/app/services/network.service.ts
+++ src/app/services/network.service.ts
@@ -7,6 +7,7 @@ import { Injectable } from '@angular/core';
 /**
  * Helper service to show how SDK tracking the network status on different platforms.
  */
+// TODO - should be part of the SDK
 export class NetworkService {
     public networkInterface: NetworkStatus;
     constructor() {
