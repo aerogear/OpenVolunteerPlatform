96c25c347a62fb116a290e69a6750b545878de8b
Dara Hayes <dara.hayes@redhat.com>
2019-07-12 09:17:59 +0100

    fix: use in memory pubsub when no MQTT_HOST set

--- server/src/subscriptions.js
+++ server/src/subscriptions.js
@@ -1,32 +1,41 @@
-const { PubSub } = require('apollo-server');
+const { PubSub } = require('apollo-server')
 const mqtt = require('mqtt')
 const { MQTTPubSub } = require('@aerogear/graphql-mqtt-subscriptions')
 
-const host = process.env.MQTT_HOST || 'localhost'
+function getPubSub() {
+  const mqttHost = process.env.MQTT_HOST
 
-const mqttOptions = {
-  host: host,
-  servername: host, // needed to work in OpenShift. Lookup SNI.
-  username: process.env.MQTT_USERNAME || '',
-  password: process.env.MQTT_PASSWORD || '' ,
-  port: process.env.MQTT_PORT || '1883',
-  protocol: process.env.MQTT_PROTOCOL || 'mqtt',
-  rejectUnauthorized: false
+  if (mqttHost) {
+    console.log('Using MQTT PubSub')
+    const mqttOptions = {
+      host: mqttHost,
+      servername: mqttHost, // needed to work in OpenShift. Lookup SNI.
+      username: process.env.MQTT_USERNAME || '',
+      password: process.env.MQTT_PASSWORD || '' ,
+      port: process.env.MQTT_PORT || '1883',
+      protocol: process.env.MQTT_PROTOCOL || 'mqtt',
+      rejectUnauthorized: false
+    }
+  
+    const client = mqtt.connect(mqttHost, mqttOptions)
+  
+    console.log(`attempting to connect to messaging service ${mqttHost}`)
+  
+    client.on('connect', () => {
+      console.log('connected to messaging service')
+    })
+  
+    client.on('error', (error) => {
+      console.log('error with mqtt connection')
+      console.log(error)
+    })
+
+    return new MQTTPubSub({ client })
+  }
+  console.log('Using In Memory PubSub')
+  return new PubSub()
 }
 
-const client = mqtt.connect(host, mqttOptions)
-
-console.log(`attempting to connect to messaging service ${host}`)
-
-client.on('connect', () => {
-  console.log('connected to messaging service')
-})
-
-client.on('error', (error) => {
-  console.log('error with mqtt connection')
-  console.log(error)
-})
-
 module.exports = {
-    pubSub: new MQTTPubSub({ client })
+    pubSub: getPubSub()
 }
