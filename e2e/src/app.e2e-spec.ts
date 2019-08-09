62fa0c3987e0ff3fd49192c0e9e2785284191efa
Davide <david3bizzarri@gmail.com>
2019-08-09 14:23:50 +0200

    fix: wait for angular before starting e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,13 @@
-import { element, by } from 'protractor';
+import { element, by, browser } from 'protractor';
 
 describe('App', () => {
+  beforeAll(async () => {
+    // Sometimes protractor is faster than angular.
+    // By making Appium sleep for 1s before injecting protractor
+    // we ensure that angular get loaded correctly.
+    await browser.sleep(1000);
+  });
+
   it('AeroGear logo should be visible', () => {
     expect(element(by.css('ion-img.logo')).isDisplayed()).toBeTruthy();
   });
