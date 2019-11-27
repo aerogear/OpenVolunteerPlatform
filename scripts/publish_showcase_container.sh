ddafb784614cbd31d4500a8cff906aba53eac522
Wojciech Trocki <wtrocki@redhat.com>
2019-11-27 14:18:07 +0100

    fix: Rename docker scripts

--- scripts/publish_showcase_container.sh
+++ scripts/publish_showcase_container.sh
@@ -2,6 +2,7 @@
 
 [ -z "$CI" ] && echo "This script is meant to run only from CircleCI." && exit 1;
 [ -z "$DOCKERHUB_USERNAME" ] && echo "Undefined DOCKERHUB_USERNAME, skipping publish" && exit 1;
+[ -z "$DOCKERHUB_PASSWORD" ] && echo "Undefined DOCKERHUB_PASSWORD, skipping publish" && exit 1;
 
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
 
