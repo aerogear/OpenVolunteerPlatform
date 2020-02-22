e173b244c3a4a9eaebb11ea30190709f7191c7ff
Wojciech Trocki <wtrocki@redhat.com>
2020-02-22 00:19:27 +0000

    fix: AMQ and keycloak ported to typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,16 @@
 import cors from 'cors';
 import express from 'express';
-import http from 'http';
+
+import { KeycloakSecurityService } from '@aerogear/voyager-keycloak'
+import { createSubscriptionServer } from '@aerogear/voyager-subscriptions'
+
+import { config } from './config/config'
+
+process.on('unhandledRejection', (error: any) => {
+  console.error(error.message, error.stack)
+  process.exit(1)
+})
+
 
 import { createApolloServer } from './graphql';
 
@@ -8,23 +18,36 @@ async function start() {
   const app = express();
 
   app.use(cors());
-
+  app.use('/', express.static('website'))
   app.get('/health', (req, res) => res.sendStatus(200));
+
+  let keycloakService;
+
+  if (config.keycloakConfig) {
+    keycloakService = new KeycloakSecurityService(config.keycloakConfig)
+    keycloakService.applyAuthMiddleware(app)
+  }
 
   const apolloServer = await createApolloServer();
   apolloServer.applyMiddleware({ app });
 
-  const httpServer = http.createServer(app);
-  apolloServer.installSubscriptionHandlers(httpServer);
-
-  const port = process.env.PORT || 4000;
-
-  httpServer.listen({ port }, () => {
-    console.log(`🚀  Server ready at http://localhost:${port}/graphql`)
+  const server = app.listen(config.port, () => {
+    createSubscriptionServer({
+      securityService: keycloakService,
+      schema: (apolloServer as unknown as any).schema
+    }, {
+      path: '/graphql',
+      server
+    })
   })
 }
 
 start().catch((err) => {
   console.error(err);
   process.exit(1);
-});
+}).then(() => {
+  console.log(`\n    ***********************************************************
+    🎮 Ionic PWA application available at http://localhost:${config.port}
+    🚀 GraphQL Playground is available at http://localhost:${config.port}/graphql
+    ***********************************************************`)
+})
