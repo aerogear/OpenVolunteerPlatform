7fa18668748a3fd9881b7261b17aa92faa7d5c5f
Wojciech Trocki <wtrocki@redhat.com>
2020-02-22 00:05:56 +0000

    fix: Add required packages and scripts

---
+++ server/scripts/keycloak/getToken.js
@@ -0,0 +1,23 @@
+const tokenRequester = require('keycloak-request-token')
+
+const username = process.argv[2]
+const password = process.argv[3]
+
+const baseUrl = 'http://localhost:8080/auth';
+const settings = {
+    username: username || 'developer',
+    password: password || 'developer',
+    grant_type: 'password',
+    client_id: 'voyager-testing-public',
+    realmName: 'voyager-testing'
+}
+
+tokenRequester(baseUrl, settings)
+  .then((token) => {
+    const headers = {
+      Authorization: `Bearer ${token}`
+    }
+    console.log(JSON.stringify(headers))
+  }).catch((err) => {
+    console.log('err', err)
+  })
