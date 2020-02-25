be1fb13a2233557ebe5a3971decda0e06e552600
Wojciech Trocki <wtrocki@redhat.com>
2020-02-25 00:20:11 +0000

    chore: minor style issues

--- client/src/graphql/queries/findTasks.ts
+++ client/src/graphql/queries/findTasks.ts
@@ -2,12 +2,11 @@ import gql from "graphql-tag"
 import { TaskExpandedFragment } from "../fragments/TaskExpanded"
 
 export const findTasks = gql`
-
   query findTasks($fields: TaskInput!) {
     findTasks(fields: $fields) {
       ...TaskExpandedFields
     }
   }
-  
+
   ${TaskExpandedFragment}
 `
