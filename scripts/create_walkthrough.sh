76f67e1d7d14acb66650e02663b714e863db7844
Wojciech Trocki <wtrocki@redhat.com>
2020-02-26 11:04:11 +0000

    fix: Improve script

--- scripts/create_walkthrough.sh
+++ scripts/create_walkthrough.sh
@@ -3,14 +3,15 @@ set -e
 
 echo "Creating branch. "
 echo "This command will reset your master branch and remove generated files"
+echo "Press any key or cancel"
 
 read choice
 
 git checkout master
 git reset --hard origin/master
-rm -Rf ./client/graphql
-rm -Rf ./server/resolvers
-rm -Rf ./server/schema
+rm -Rf ./client/src/graphql
+rm -Rf ./server/src/resolvers
+rm -Rf ./server/src/schema
 git add --all
 git commit -m"Walktrough cleanup"
 git push origin +walkthrough
