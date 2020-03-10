5a1ffa15820dec7b54c6d65905e0be66a77764a2
Wojciech Trocki <wtrocki@redhat.com>
2020-03-10 13:04:43 +0000

    fix: Use showcase as mongo collection

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -12,7 +12,7 @@ export class Config {
     this.port = process.env.PORT || 4000
 
     this.db = {
-      database: process.env.MONGO_COLLECTION || 'tasks',
+      database: process.env.MONGO_COLLECTION || 'showcase',
       host: process.env.MONGO_HOST || '127.0.0.1',
       user: process.env.MONGO_USER,
       password: process.env.MONGO_PASSWORD,
