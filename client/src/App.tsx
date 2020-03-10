53b1648e84817bdf87526617068001f39a27562f
Dara Hayes <dara.s.hayes@gmail.com>
2020-03-10 11:47:25 +0000

    feat: show toast when conflicts occur

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Router } from './components';
+import { useApolloOfflineClient } from "react-offix-hooks";
 
 // load all styles
 import './theme';
+import { ConflictListener } from 'offix-client';
+import { IonToast } from '@ionic/react';
 
 export const App: React.FC = () => {
 
-  return <Router />;
-  
+  const client = useApolloOfflineClient();
+  const [showConflict, setShowConflict] = useState(false);
+
+  useEffect(() => {
+    const conflictListener: ConflictListener = {
+      mergeOccurred() {
+        setShowConflict(true);
+      },
+      conflictOccurred() {
+        setShowConflict(true);
+      }
+    };
+    client.addConflictListener(conflictListener);
+
+    return function cleanup() {
+      client.removeConflictListener(conflictListener);
+    }
+  }, [client]);
+
+  return (
+    <>
+      <Router />
+      <IonToast
+        isOpen={showConflict}
+        onDidDismiss={() => setShowConflict(false)}
+        message="Conflict Occurred 👌👌👌"
+        duration={2000}
+      />
+    </>
+  );
 };
 
