c12310fffd9182dc1f006b0aba08c6ec1547bac1
Wojciech Trocki <wtrocki@redhat.com>
2020-03-16 12:40:39 +0000

    fix: Android emulator support for server url

--- client/src/config/clientConfig.ts
+++ client/src/config/clientConfig.ts
@@ -7,9 +7,16 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { globalCacheUpdates, ConflictLogger } from '../helpers';
 import { getAuthHeader } from '../auth/keycloakAuth';
 import { ApolloOfflineClientOptions } from 'offix-client';
+import { Capacitor } from '@capacitor/core';
+
 
 let httpUri = 'http://localhost:4000/graphql';
 let wsUri = 'ws://localhost:4000/graphql';
+
+if (Capacitor.isNative && Capacitor.platform === 'android') {
+  httpUri = 'http://10.0.2.2:4000/graphql';
+  wsUri = 'ws://10.0.2.2:4000/graphql';
+}
 
 if (process.env.REACT_APP_URI_FORMAT === 'RELATIVEURI') {
   httpUri = "/graphql";
