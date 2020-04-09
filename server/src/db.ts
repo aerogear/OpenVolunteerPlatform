90bdf54d684c15cb23a7d572987f190ae5453b7b
Wojciech Trocki <wtrocki@redhat.com>
2020-04-09 22:37:58 +0100

    db migrations/restore

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,21 +1,35 @@
 import { Config } from './config/config';
 import e = require('express');
+import { resolve } from 'path';
 
 const MongoClient = require('mongodb').MongoClient;
 
 export async function connect(config: Config) {
-    // TODO config
-    let url: string;
+  // TODO config
+  let url: string;
 
-    if (config.db.user && config.db.password) {
-      url = `mongodb://${config.db.user}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.database}`;
-    } else {
-      url = `mongodb://${config.db.host}:${config.db.port}/${config.db.database}`
-    }
+  if (config.db.user && config.db.password) {
+    url = `mongodb://${config.db.user}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.database}`;
+  } else {
+    url = `mongodb://${config.db.host}:${config.db.port}/${config.db.database}`
+  }
 
-    // Use connect method to connect to the server
-    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-    const db = client.db(config.db.database);
-    return db;
+  // var backup = require('mongodb-backup');
+
+  // backup({
+  //   uri: url,
+  //   root: resolve(__dirname, '../mongodump')
+  // });
+
+  var restore = require('mongodb-restore');
+  restore({
+    uri: url,
+    root: resolve(__dirname, '../mongodump/showcase')
+  });
+
+  // Use connect method to connect to the server
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  const db = client.db(config.db.database);
+  return db;
 }
 
