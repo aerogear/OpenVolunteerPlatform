b02940b7fadf974c469fd9b4b318501c56a451ca
Wojciech Trocki <wtrocki@redhat.com>
2020-04-15 12:46:57 +0100

    fix: delete not needed queries and files

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,3 @@
 export { TaskPage } from './TaskPage';
 export { ProfilePage } from './ProfilePage';
 export { ViewTaskPage } from './ViewTaskPage';
-export { OfflineQueuePage } from './OfflineQueuePage';
-
