1d7df583f621c39fb8588d043c3ad6c6da4e2b32
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2020-05-15 15:49:46 +0200

    fix: set version field when creating model

--- platform/server/src/mongo/createMongoServices.ts
+++ platform/server/src/mongo/createMongoServices.ts
@@ -2,7 +2,7 @@ import { GraphQLObjectType, GraphQLSchema } from "graphql"
 import { PubSubEngine } from 'graphql-subscriptions';
 import { CRUDService, GraphbackPubSubModel } from "@graphback/runtime"
 import { Db } from "mongodb"
-import { MongoDBDataProvider } from "./MongoDBDataProvider";
+import { OffixMongoDBDataProvider } from '@graphback/runtime-mongo';
 
 /**
  * Helper function for creating mongodb runtime context used in Graphback
@@ -27,7 +27,7 @@ export const createMongoCRUDRuntimeContext = (
       Please make sure that you pass the right schema to createCRUDRuntimeContext`)
     }
 
-    const objectDB = new MongoDBDataProvider(modelType, db)
+    const objectDB = new OffixMongoDBDataProvider(modelType, db)
 
     services[model.name] = new CRUDService(modelType, objectDB, {
       pubSub,
