43f69845be57909469da4e5b850bc1ce4185d5f9
Wojciech Trocki <wtrocki@redhat.com>
2020-06-26 13:20:43 +0100

    fix: Auth rules with config

---
+++ platform/server/src/config/auth.ts
@@ -0,0 +1,34 @@
+import { CrudServicesAuthConfig } from "@graphback/keycloak-authz"
+
+export const authConfig: CrudServicesAuthConfig = {
+  DistributionCentre: {
+    create: { roles: ['admin'] },
+    read: { roles: [] },
+    update: { roles: ['admin'] },
+    delete: { roles: ['admin'] },
+  },
+  Volunteer: {
+    create: { roles: ['admin'] },
+    read: { roles: [] },
+    update: { roles: [] },
+    delete: { roles: ['admin'] },
+  },
+  Recipient: {
+    create: { roles: ['admin'] },
+    read: { roles: [] },
+    update: { roles: ['admin'] },
+    delete: { roles: ['admin'] },
+  },
+  VolunteerAction: {
+    create: { roles: ['admin'] },
+    read: { roles: [] },
+    update: { roles: ['admin'] },
+    delete: { roles: ['admin'] },
+  },
+  VolunteerActionProduct: {
+    create: { roles: ['admin'] },
+    read: { roles: [] },
+    update: { roles: ['admin'] },
+    delete: { roles: ['admin'] },
+  },
+}
