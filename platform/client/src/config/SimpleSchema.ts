5f4dfc0a7943b03c6e019c14799f0bea5fb3dbf8
Wojtek Trocki <wtrocki@redhat.com>
2020-07-06 16:57:28 +0100

    fix: Remove redundant registration

--- platform/client/src/config/SimpleSchema.ts
+++ platform/client/src/config/SimpleSchema.ts
@@ -4,25 +4,4 @@ import { filterDOMProps } from 'uniforms';
 // Register custom property.
 SimpleSchema.extendOptions(['uniforms']);
 
-// There's no possibility to retrieve them at runtime
-filterDOMProps.register(
-  'allowedValues',
-  'autoValue',
-  'blackbox',
-  'custom',
-  'decimal',
-  'defaultValue',
-  'exclusiveMax',
-  'exclusiveMin',
-  'label',
-  'max',
-  'maxCount',
-  'min',
-  'minCount',
-  'optional',
-  'regEx',
-  'trim',
-  'type',
-);
-
 export default SimpleSchema;
