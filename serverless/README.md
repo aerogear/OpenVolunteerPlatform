e763aab0178c8bb2f94208adc2ab27c9c677900b
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2020-10-14 13:17:04 +0200

    feat: add a knative express brique that serves dailyactionplan

---
+++ serverless/README.md
@@ -0,0 +1,31 @@
+
+## Deploy
+
+### Create the handler for configmap
+
+Modify the `handler.ts` file to suite your needs. 
+
+1. Compile the project
+
+```bash
+yarn build
+```
+2. Create the configmap
+```bash
+oc create configmap knative-handker --from-file=handler=dist/handler.js
+```
+
+### Deploy the service
+
+Define the MongoDB connection string 
+
+```
+MONGO_CONNECTION=mongodb://user:password@mongodb/showcase
+./deploy.sh
+```
+
+### Get route of the deployed service
+
+```bash
+oc get ksvc | grep 'ovp-knative' | awk '{ print $2 }'
+```
--- serverless/src/index.ts
+++ serverless/src/index.ts
@@ -3,20 +3,16 @@ require('dotenv').config();
 import cors from 'cors';
 import express from 'express';
 import http from 'http';
-import { loadDBConfig, connectDB } from './db';
 import { handler } from "./handler"
 const app = express();
 
 app.use(cors());
-
-const db = connectDB();
-const dbConfig = loadDBConfig();
 
 // TODO use file that is replaced by config map
 app.post("/handler", handler)
 const httpServer = http.createServer(app);
 
 
-httpServer.listen({ port: 8100 }, () => {
+httpServer.listen({ port: +(process.env.PORT || 8100) }, () => {
   console.log(`🚀  handler ready at http://localhost:8100/handler`);
 });
