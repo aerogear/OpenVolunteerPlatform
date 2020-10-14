5c4282de7b99980a81e387a226caa7186b3cf8d5
Enda Phelan <ephelan@redhat.com>
2020-10-14 08:55:46 +0100

    chore: add datasync-start project

---
+++ platform/datasync-server/functions/typescript/taskResolvers.ts
@@ -0,0 +1,21 @@
+import { IResolvers, GraphQLResolveInfo, QueryFilter, GraphbackContext } from './generated';
+import { NoteFilter } from './generated/generated-types';
+
+/**
+ * getDraftTasks serverless function
+ */
+export const noteResolvers: IResolvers = {
+  Query: {
+    getDraftTasks: async (parent: any, args: any, context: GraphbackContext, info: GraphQLResolveInfo) => {
+      const filter: QueryFilter<NoteFilter> = {
+        title: {
+          startsWith: 'Draft'
+        }
+      }
+
+      const results = await context.graphback.Task.findBy({ filter }, context, info);
+
+      return results.items;
+    }
+  }
+}
